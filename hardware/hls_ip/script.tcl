f7ba6516c321ffe692ce4a54551ddd743b4d648c
Shane Fleming <sf306@imperial.ac.uk>
2014-10-25 20:24:58 +0100

    Moving the src directory to higher up

--- hardware/hls_ip/script.tcl
+++ hardware/hls_ip/script.tcl
@@ -5,8 +5,8 @@
 ############################################################
 open_project hls_ip
 set_top image_filter
-add_files hls_ip/src/top.cpp
-add_files hls_ip/src/top.h
+add_files ./src/top.cpp
+add_files ./src/top.h
 open_solution "solution"
 set_part {xc7z020clg484-1}
 create_clock -period 10 -name default
